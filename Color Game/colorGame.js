318a56d27d439cf670a65f34e2e0f700afc33623
Oleksii Mostovyi <oleksii.mostovyi@globallogic.com>
2018-08-05 14:57:26 +0300

    finish

--- Color Game/colorGame.js
+++ Color Game/colorGame.js
@@ -1,20 +1,54 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor(colors);
 var pickedDispColor = document.querySelector('#colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easyBtn');
+var heardBtn = document.querySelector('#heardBtn');
+var btns = document.querySelectorAll('button');
 
+easyBtn.addEventListener('click', function () {
+    easyBtn.classList.add('selected');
+    heardBtn.classList.remove('selected');
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor(colors);
+    pickedDispColor = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
+});
+
+heardBtn.addEventListener('click', function () {
+    heardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor(colors);
+    pickedDispColor = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+        squares[i].style.display = 'block';
+    }
+});
 
 pickedDispColor.textContent = pickedColor;
-for (var i = 0; i < squares.length; i++) {
+
+for (let i = 0; i < squares.length; i++) {
     // add initial colors to squares
     squares[i].style.backgroundColor = this.colors[i];
-    
+
     // add event listeners to squares
     squares[i].addEventListener('click', function () {
         // grab color of clicked square
-        var clickedColor = this.style.backgroundColor;
+        let clickedColor = this.style.backgroundColor;
 
         // compare color to picked color
         if (clickedColor === pickedColor) {
@@ -24,49 +58,72 @@ for (var i = 0; i < squares.length; i++) {
             messageDisplay.textContent = 'Try Again';
         }
     });
-    
 }
 
+resetButton.addEventListener('click', function () {
+    resetButton.textContent = 'New Colors';
+    h1.style.backgroundColor = 'steelblue';
+    // generate new set of colors
+    colors = generateRandomColors(numSquares);
+
+    // pick a new random color
+    pickedColor = pickColor(colors);
+    pickedDispColor.textContent = pickedColor;
+    this.textContent = 'New Colors';
+
+    messageDisplay.textContent = "";
+    // change colors of squaresl
+    for (let i = 0; i < squares.length; i++) {
+        // add initial colors to squares
+        squares[i].style.backgroundColor = colors[i];
+    }
+});
+
+// get a random number from 0 to 255
 function randInt() {
     return Math.floor(Math.random() * 256);
-    console.log('random: ', Math.floor(Math.random() * 256););
-
 }
 
-
-// function is not finished. need debugging
-function generateRandomColors(num) {
-    // make an array
-    var arr = [];
-    // add num random colors to array
-    for (var i = 0; i < num;i++) {
-        var resArr = [];
-        for (var j = 0; j < 3; j++) {
-            resArr.push(randInt());
-            console.log(resArr.push(randInt()));
-        }
-        var res = 'rgb(' + arr.join(', ') + ')'
+// generate one color
+function colorOne() {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+        arr.push(randInt());
     }
-    arr.push(res);
-    // return that array
+    return 'rgb(' + arr.join(', ') + ')';
+}
+
+// generate a set of colors
+function generateRandomColors(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(colorOne());
+    }
     return arr;
 }
-
+// pick a random color from the generated set
 function pickColor(arr) {
-    var randChoosed = Math.floor(Math.random() * colors.length);
+    let randChoosed = Math.floor(Math.random() * colors.length);
     console.log('randChoosed: ', randChoosed);
+    console.log('picked color is: ', arr[randChoosed]);
+    console.log('colors: ', colors);
 
     return arr[randChoosed];
-    console.log('picked color is ', arr[randChoosed]);
+    // console.log('picked color is ', arr[randChoosed]);
 
 }
-
+// if user quessed color makes all squares and header of this color
 function changeColors(color) {
     messageDisplay.textContent = 'Correct!!!';
+    resetButton.textContent = 'PLAY AGAIN?';
     squares.forEach(function (element) {
         element.style.backgroundColor = color;
         h1.style.backgroundColor = color;
+        var winnBtnStyle = document.createElement('style');
+        winnBtnStyle.type = 'text/css';
+        winnBtnStyle.innerHTML = 'button:hover {color: whitesmoke; background: color;}'
     });
 }
+
 
 
